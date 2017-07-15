b7beebc20f62d2fb01094382e636e28fa56cbd01
Julian Ospald <hasufell@posteo.de>
2017-07-15 11:19:55 +0200

    Fix extern crate/mod visibility

--- src/lib.rs
+++ src/lib.rs
@@ -34,57 +34,57 @@ extern crate alloc_system;
 
 pub extern crate flexi_logger;
 #[macro_use]
-pub extern crate log;
+extern crate log;
 
 #[macro_use]
-pub extern crate error_chain;
+extern crate error_chain;
 
 #[macro_use]
-pub extern crate serde_derive;
-pub extern crate toml;
-pub extern crate serde;
+extern crate serde_derive;
+extern crate toml;
+extern crate serde;
 
-pub extern crate alsa;
-pub extern crate alsa_sys;
-pub extern crate ffi;
-pub extern crate gdk;
-pub extern crate gdk_pixbuf;
-pub extern crate gdk_pixbuf_sys;
-pub extern crate gdk_sys;
-pub extern crate gio;
-pub extern crate glib;
-pub extern crate glib_sys;
-pub extern crate gobject_sys;
+extern crate alsa;
+extern crate alsa_sys;
+extern crate ffi;
+extern crate gdk;
+extern crate gdk_pixbuf;
+extern crate gdk_pixbuf_sys;
+extern crate gdk_sys;
+extern crate gio;
+extern crate glib;
+extern crate glib_sys;
+extern crate gobject_sys;
 pub extern crate gtk;
-pub extern crate gtk_sys;
-pub extern crate libc;
-pub extern crate png;
-pub extern crate which;
-pub extern crate xdg;
+extern crate gtk_sys;
+extern crate libc;
+extern crate png;
+extern crate which;
+extern crate xdg;
 
 #[cfg(feature = "notify")]
-pub extern crate libnotify;
+extern crate libnotify;
 
 #[macro_use]
-pub mod errors;
+mod errors;
 
 #[macro_use]
-pub mod glade_helpers;
+mod glade_helpers;
 
-pub mod alsa_card;
+mod alsa_card;
 pub mod app_state;
-pub mod audio;
-pub mod prefs;
-pub mod support_alsa;
-pub mod support_audio;
-pub mod support_cmd;
+mod audio;
+mod prefs;
+mod support_alsa;
+mod support_audio;
+mod support_cmd;
 #[macro_use]
-pub mod support_ui;
+mod support_ui;
 pub mod ui_entry;
-pub mod ui_popup_menu;
-pub mod ui_popup_window;
-pub mod ui_prefs_dialog;
-pub mod ui_tray_icon;
+mod ui_popup_menu;
+mod ui_popup_window;
+mod ui_prefs_dialog;
+mod ui_tray_icon;
 
 #[cfg(feature = "notify")]
-pub mod notif;
+mod notif;
